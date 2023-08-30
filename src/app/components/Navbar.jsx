c44acac7c11d2f4ed2bfea9a14df8a24cc5de0d4
Harun Yıldız <yildizharun@outlook.com>
2023-08-30 22:40:15 +0300

    Add: navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { useSelector } from "react-redux";
+
+export default function Navbar() {
+  const posts = useSelector((state) => state.posts || []);
+
+  return (
+    <nav className="bg-blue-500 p-4 text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo and Company Name */}
+        <div className="flex items-center">
+          <img src="logo.png" alt="Company Logo" className="w-8 h-8 mr-2" />
+          <span className="text-xl">Company Name</span>
+        </div>
+
+        {/* Posts Count */}
+        <div className="flex items-center">
+          <span className="mr-4">Posts ({posts.length})</span>
+
+          {/* Icons */}
+          <span className="material-icons mr-4">icon_name1</span>
+          <span className="material-icons mr-4">icon_name2</span>
+
+          {/* Profile Image */}
+          <img
+            src="profile_image.png"
+            alt="Profile"
+            className="w-8 h-8 rounded-full"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Providers } from "./GlobalRedux/provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
