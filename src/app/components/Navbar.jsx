3cb8e4530f5b451760149430280ccaa007c78da6
Harun <harun.yildiz@oroox.com>
2023-08-31 11:56:07 +0300

    Update: styling

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 import { ImBlogger } from "react-icons/im";
+import { GoBellFill } from "react-icons/go";
+import { BsFillGridFill } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
 
 export default function Navbar() {
   const posts = useSelector((state) => state.posts || []);
 
   return (
-    <nav className="bg-white p-3 text-black">
+    <nav className="bg-white p-3 text-black rounded-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
           {/* Logo  */}
@@ -17,7 +20,7 @@ export default function Navbar() {
           </div>
           {/* Company Name */}
           <Link
-            className="text-2xl font-semibold text-black hover:text-primary"
+            className="text-2xl font-bold text-black hover:text-primary"
             href="/"
           >
             Arbit Blog
@@ -25,19 +28,16 @@ export default function Navbar() {
         </div>
 
         {/* Posts Count */}
-        <div className="flex items-center">
-          <span className="mr-4">Posts ({posts.length})</span>
-
-          {/* Icons */}
-          <span className="material-icons mr-4">icon_name1</span>
-          <span className="material-icons mr-4">icon_name2</span>
-
-          {/* Profile Image */}
-          <img
-            src="profile_image.png"
-            alt="Profile"
-            className="w-8 h-8 rounded-full"
-          />
+        <div className="flex items-center relative">
+          <div className="mx-2 relative">
+            <span className="text-gray-500 font-bold text-xl">Posts</span>
+            <span className="absolute -top-4 -right-1 bg-red-500 text-white rounded-full text-sm px-1">
+              10
+            </span>
+          </div>
+          <GoBellFill className="text-gray-500 text-2xl mx-2" />
+          <BsFillGridFill className="text-gray-500 text-2xl mx-2" />
+          <CgProfile className="text-primary text-5xl mx-2" />
         </div>
       </div>
     </nav>
